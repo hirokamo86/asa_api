95c50a812a92cd774caa6a1dc59f668df33f3fe5
hirokamo <hirokamo@localhost.localdomain>
2016-03-26 16:48:19 +0900

    using Object feature

--- acl/views.py
+++ acl/views.py
@@ -24,20 +24,22 @@ def logging_in(request):
     return redirect('acl:list')
     
 def list(request):
-    ace_dict = get_ace_dict(ip_address, username, password)
+    outside_ace_dict = get_ace_dict(ip_address, username, password, "outside")
+    malhosts_og_dict = get_og_dict(ip_address, username, password, "MALICIOUS_HOSTS")
     context = RequestContext(request, {'ip_address': ip_address,
                                        'username': username,
                                        'password': password,
-                                       'ace_dict': ace_dict,
+                                       'outside_ace_dict': outside_ace_dict,
+                                       'malhosts_og_dict': malhosts_og_dict,
                                        })
     return render_to_response('acl/list.html', context_instance=context)
 
 def add(request):
-    add_ace()
+    add_no()
     return redirect('acl:list')
 
 def delete(request):
-    delete_ace()
+    delete_no()
     return redirect('acl:list')
 
 def logout(request):
@@ -45,11 +47,11 @@ def logout(request):
     return render_to_response('acl/login.html', context_instance=context)
 
 #functions
-def get_ace_dict(ip_address, username, password):
+def get_ace_dict(ip_address, username, password, nameif):
         
     headers = {'Content-Type': 'application/json'}
     
-    api_path = "/api/access/out/outside/rules/"
+    api_path = "/api/access/out/" + nameif + "/rules/"
     url = "https://" + ip_address + api_path
     f = None
     
@@ -66,8 +68,29 @@ def get_ace_dict(ip_address, username, password):
     finally:
         if f:  f.close()
 
-def add_ace():
+def get_og_dict(ip_address, username, password, og_name):
+    
+    headers = {'Content-Type': 'application/json'}
+
+    api_path = "/api/objects/networkobjectgroups/" + og_name        
+    url = "https://" + ip_address + api_path
+    f = None
+    
+    req = urllib2.Request(url, None, headers)
+    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+    req.add_header("Authorization", "Basic %s" % base64string)
+    try:
+        f = urllib2.urlopen(req)
+        status_code = f.getcode()
+        if (status_code != 200):
+            return 'Error in get. Got status code: '+status_code
+        resp = f.read()
+        return json.loads(resp)
+    finally:
+        if f:  f.close()
+
+def add_no():
     print ""
 
-def delete_ace():
+def delete_no():
     print ""
