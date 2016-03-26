c58b56dd29b247a4b6655a959a7269a7bbcff716
hirokamo <hirokamo@localhost.localdomain>
2016-03-26 17:33:22 +0900

    using Object feature / added add function

--- acl/views.py
+++ acl/views.py
@@ -4,7 +4,6 @@ from django.template import RequestContext
 
 import base64
 import json
-import sys
 import urllib2
 
 ip_address = ''
@@ -35,7 +34,9 @@ def list(request):
     return render_to_response('acl/list.html', context_instance=context)
 
 def add(request):
-    add_no()
+    kind = request.POST['kind']
+    value = request.POST['value']
+    add_no(ip_address, username, password, "MALICIOUS_HOSTS", kind, value)
     return redirect('acl:list')
 
 def delete(request):
@@ -89,8 +90,42 @@ def get_og_dict(ip_address, username, password, og_name):
     finally:
         if f:  f.close()
 
-def add_no():
-    print ""
+def add_no(ip_address, username, password, og_name, kind, value):
+    
+    headers = {'Content-Type': 'application/json'}
+
+    api_path = "/api/objects/networkobjectgroups/" + og_name
+    url = "https://" + ip_address + api_path
+    f = None
+    
+    put_data = {
+      "members.add": [
+        {
+          "kind": kind,
+          "value": value
+        }
+      ]
+    }
+    req = urllib2.Request(url, json.dumps(put_data), headers)
+    base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+    req.add_header("Authorization", "Basic %s" % base64string)   
+    req.get_method = lambda: 'PATCH'
+    try:
+        f = urllib2.urlopen(req)
+        status_code = f.getcode()
+        print "Status code is "+str(status_code)
+        if status_code == 204:
+            return True
+    except urllib2.HTTPError, err:
+        print "Error received from server. HTTP Status code :"+str(err.code)
+        try:
+            json_error = json.loads(err.read())
+            if json_error:
+                print json.dumps(json_error,sort_keys=True,indent=4, separators=(',', ': '))
+        except ValueError:
+            pass
+    finally:
+        if f:  f.close()
 
 def delete_no():
     print ""
